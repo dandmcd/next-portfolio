dc2a1bb91a1a07a3eaacc73ab3e9252db17f1a45
Daniel McDermott <daniel@shineforth.co>
2024-09-15 10:18:28 +0800

    feature/2024-09-15-dependency-updates Updated font import and removed bg image

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Barlow_Semi_Condensed } from "@next/font/google";
+import { Barlow_Semi_Condensed } from "next/font/google";
 import styled, { ThemeProvider } from "styled-components";
 import { GlobalStyle, theme } from "../styles/globalstyled";
 import Footer from "./Footer/Footer";
@@ -9,19 +9,11 @@ const barlow = Barlow_Semi_Condensed({
   subsets: ["latin"],
 });
 
-const BgImage = styled(Image)`
-  z-index: -1;
-  object-fit: cover;
-  background-size: cover;
-  background-repeat: no-repeat;
-`;
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <BgImage src="/wavepattern6.svg" alt="Background" fill priority />
         <main className={barlow.className}>{children}</main>
         <Footer />
       </ThemeProvider>
