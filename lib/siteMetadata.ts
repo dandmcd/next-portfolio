6c1af81f87ffedcf25c3ebddc84267937727b24f
Daniel McDermott <daniel@shineforth.co>
2024-09-29 12:07:32 +0800

    Update title branding

--- lib/siteMetadata.ts
+++ lib/siteMetadata.ts
@@ -3,7 +3,7 @@ const siteMetadata = {
   description: "Bringing ideas to life back to front",
   siteLogo: "/public/computerdan.jpg",
   email: "mail@danielmcdermott.me",
-  title: "Daniel.Me",
+  title: "Daniel McDermott | Developer",
 };
 
 export default siteMetadata;
